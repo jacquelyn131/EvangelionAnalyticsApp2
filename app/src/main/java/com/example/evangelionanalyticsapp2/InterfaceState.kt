5e66cda7a91f8bd96f427c64896134a79484a596
Jacquelyn Shelby <jacquelyns131@gmail.com>
2023-08-06 04:23:53 -0400

    InterfaceState class
    - class to keep track of current state

--- app/src/main/java/com/example/evangelionanalyticsapp2/InterfaceState.kt
+++ app/src/main/java/com/example/evangelionanalyticsapp2/InterfaceState.kt
@@ -2,6 +2,15 @@ package com.example.evangelionanalyticsapp2
 
 class InterfaceState {
     var state = 0
+    fun InterfaceState() {
+        state = 0
+    }
+    fun getState(): Int {
+        return state
+    }
+    fun setState(s: Int) {
+        state = s
+    }
 
 
 }
